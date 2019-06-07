63ccdd3265d7805d74a4141c40b4f1005fca1569
zihua3 <1162846739@qq.com>
2019-06-07 16:51:27 +0800

    设置默认路由，去掉地址中的#号

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,10 @@ import Route from "./base/route";
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
-    Route.create("helloworld", "/", "/helloworld.vue"),
+    Route.create("helloworld", "/", "", {redirect: '/home'}),
+    Route.create("helloworld", "/home", "/helloworld.vue"),
     Route.create("helloworld1", "/helloworld", "/helloworld1.vue"),
   ]
 })
